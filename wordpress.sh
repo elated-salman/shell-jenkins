89af5aa3320bf3ad1b0704d72be78e954e80fe96
Salman <59947811+elated-salman@users.noreply.github.com>
2023-05-24 12:09:09 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -6,5 +6,5 @@ sudo amazon-linux-extras install php7.4
 sudo yum install -y php-cli php-pdo php-fpm php-json php-mysqlnd
 sudo yum install -y mysql 
 sudo yum install -y git
-sudo git clone https://github.com/saitejyellina/wordpress.git /var/www/html
+sudo git clone https://https://github.com/elated-salman/shell-jenkins.git /var/www/html
 sudo systemctl restart httpd
